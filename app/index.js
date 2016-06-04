386e813ea09e20dba3959b7299daa9b38ea7f669
Alberto Lopez <albertolopez19@gmail.com>
2016-06-03 23:31:44 -0400

    Refactor to webpack and sass

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import VanillaModal from 'vanilla-modal';
+import Animate from './animate.js';
+import css from '../style/main.scss';
+
+Animate();
+
+const options = {
+	
+};
+const modal = new VanillaModal(options);
---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const pkg = require('./package.json');
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build'),
+	style: path.join(__dirname, 'style')
+};
+
+const common = {
+	entry: {
+		app: PATHS.app
+		// style: "./style/main.scss"
+	},
+	resolve: {
+		extensions: ['', '.js', '.scss']
+	},
+	output: {
+		path: path.resolve(__dirname + 'build'),
+	 	publicPath: '/build/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				loaders: ['babel'],
+				include: PATHS.app
+			}
+    ]
+	},
+	sassLoader: {
+		includePaths: [path.resolve(__dirname, 'node_modules')]
+	},
+	// plugins: [
+	// 	new CleanPlugin([PATHS.build])
+	// ]
+};
+
+// Default config
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+		devServer: {
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			stats: 'errors-only',
+			contentBase: './build/',
+
+			// host and port are from env
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		module: {
+			loaders: [
+			{
+				test: /\.scss$/,
+				loaders: ['style', 'css', 'sass'],
+				include: PATHS.style
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.style
+			}
+			]
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+
+if (TARGET === 'build' || TARGET === 'stats') {
+	module.exports = merge(common, {
+		entry: {
+			vendor: Object.keys(pkg.dependencies).filter(function (v) {
+				// exclude alt-utils
+				return v;
+			})
+		},
+		output: {
+			path: PATHS.build,
+			filename: '[name].[chunkhash].js',
+			chunkFilename: '[chunkhash].js'
+		},
+		module: {
+			loaders: [{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract('style', 'css!sass')
+			}]
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': '"production"'
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			}),
+			new webpack.optimize.CommonsChunkPlugin({
+				names: ['vendor', 'manifest']
+			}),
+			new ExtractTextPlugin('[name].[chunkhash].css')
+		]
+	});
+}
